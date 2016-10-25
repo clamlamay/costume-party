036bc50d05593eb267a3eff9f9a016ec8b2eb17c
Christopher Lamlamay <Christopher@Christophers-MacBook-Pro.local>
2016-10-25 15:05:52 -0500

    fixed error and added a table

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,39 @@
 var express = require('express');
 var router = express.Router();
+//include our PartyModel
+var PartyModel = require('../models/Party');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/home', renderSQL);
+router.get('/:id', renderPartyById);
+
+
+function renderPartyById(req, res, next){
+	//call my individual model 
+	//first, I need the ID from req.params
+	var id = parseInt(req.params.id);
+	console.log(typeof id);
+	if (typeof id != 'number'){
+		res.json({message: 'Invalid ID specified'});
+	}
+	PartyModel.where({
+		id: id
+	}).fetch().then(function(model){
+		console.log(model);
+		res.render('party', model.attributes);
+	});
+};
+
+function renderSQL(req, res, next){
+	//call my collection of rows (table)
+	PartyModel.collection().fetch().then(function(models){
+		// res.render('all', models);
+		res.json(models);
+	});
+};
 
 module.exports = router;
